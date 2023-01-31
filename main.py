d0aaf8e49e4a791131870977f7d1238387f52bdb
tomo <tomo@crowdeo.com>
2023-01-31 19:43:41 +0700

    Register by app_id, addr is backup. Implement checker confirming registration nicks match up. Need a get_nick for the Blog contract for that.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Literal
 from pyteal import *
 from beaker import *
 from beaker.lib.storage import Mapping, List
+from beaker.application import get_method_signature
 from algosdk.encoding import decode_address, encode_address
 
 from pprint import pprint
@@ -19,6 +20,7 @@ MAX_USERS = 20
 class UserRecord(abi.NamedTuple):
     nick: abi.Field[Nickname]
     username: abi.Field[Username]
+    app_id: abi.Field[abi.Uint64]
     addr: abi.Field[abi.Address] # A 32-byte Algorand address. This is an alias for abi.StaticArray[abi.Byte, Literal[32]].
     canonical_uri: abi.Field[abi.String]
 
@@ -39,10 +41,10 @@ class AlgoBlogRegistry(Application):
         )
     
     @external
-    def register(self, nick: Nickname, username: Username, app_addr: abi.Address, canonical_uri: abi.String, *, output: abi.String):
+    def register(self, nick: Nickname, username: Username, app_id: abi.Uint64, app_addr: abi.Address, canonical_uri: abi.String, *, output: abi.String):
         # TODO check existing nicks
         return Seq(
-            (user := UserRecord()).set(nick, username, app_addr, canonical_uri),
+            (user := UserRecord()).set(nick, username, app_id, app_addr, canonical_uri),
             self.registry[nick.get()].set(user),
             user_count := App.box_get(Bytes("user_count")),
             Assert(user_count.hasValue()),
@@ -63,7 +65,7 @@ class AlgoBlogRegistry(Application):
             self.usernames[idx.get()].store_into(output)
         )
     
-def regdemo():
+def deploy_registry():
     deploy = 1
 
     if deploy:
@@ -81,8 +83,8 @@ def regdemo():
         """
         )
     else:
-        app_addr = 'M45TDQCC5VGD2T4Q7YRULJ6S65XOW4SD5LRWKPHGIQ5X3JNH2V7NYBFODY'
-        app_id = 983
+        app_addr = 'NCYBSXIYSDOIJ5ZB2A7B7UJXKAWJJN5XRUK5PQ26L3MFS5S6FLVZJR4FOM'
+        app_id = 1217
         app_client = client.ApplicationClient(
             client=sandbox.get_algod_client(),
             app=AlgoBlogRegistry(version=8),
@@ -91,16 +93,25 @@ def regdemo():
         )
 
     app_client.fund(517000, app_addr)
-
-    someaddr = decode_address('M45TDQCC5VGD2T4Q7YRULJ6S65XOW4SD5LRWKPHGIQ5X3JNH2V7NYBFODY')
     boxen = lambda x: [[app_client.app_id, name] for name in x]
+
     result = app_client.call(AlgoBlogRegistry.bootstrap,
                               boxes=boxen(['user_count', 'usernames']))
     print("bootstrap result: ", result.return_value) 
+
+    return app_client
+
+def regdemo():
+    app_client = deploy_registry()
+    boxen = lambda x: [[app_client.app_id, name] for name in x]
+    someappid = 1304
+    someaddr = decode_address(app_client.app_addr)
+
     # tomo = b"tomo\x00\x00\x00\x00" # 8 byte nickname
     tomo8 = _nick(b'tomo')
     tomo15 = _username(b'tomo')
     result = app_client.call(AlgoBlogRegistry.register, boxes=boxen(['user_count', 'usernames', tomo8]),
+                              app_id=someappid,
                               nick=tomo8, username=tomo15, app_addr=someaddr, canonical_uri="uri://example")
     print("register result: ", result.return_value) 
     result = app_client.call(AlgoBlogRegistry.get_user, boxes=boxen([tomo8]) , nick=tomo8)
@@ -114,27 +125,102 @@ def regdemo():
 
     result = app_client.call(AlgoBlogRegistry.register, boxes=boxen(['user_count', 'usernames', b'root1234']),
                               nick=_nick(b'root1234'), username=_username(b"root1234"),
+                              app_id=someappid,
                               app_addr=someaddr, canonical_uri="uri://example")
     result = app_client.call(AlgoBlogRegistry.register, boxes=boxen(['user_count', 'usernames', b'satoshi ']),
-                              nick=b'satoshi ', username=_username(b"satoshi"), app_addr=someaddr, canonical_uri="uri://example")
+                              app_id=someappid,
+                              nick=b'satoshi ', username=_username(b"satoshi"),  app_addr=someaddr, canonical_uri="uri://example")
     result = app_client.call(AlgoBlogRegistry.get_user, boxes=boxen([b'satoshi ']) , nick=b'satoshi ')
     print("get_user: ", result.return_value)
     result = app_client.call(AlgoBlogRegistry.get_nick_by_idx, boxes=boxen(['usernames']) , idx=2)
     print("get_nick_by_idx ", ''.join([chr(x) for x in result.return_value]))
 
-    return { app_client: app_client, app_id: app_id, app_addr: app_addr }
 
-from beaker.application import get_method_signature
+class Checker(Application):
+    @external
+    def one(self, *, output: abi.Uint8):
+        return Seq(
+            output.set(Int(1))
+        )
 
-# https://programtalk.com/python-more-examples/pyteal.InnerTxnBuilder.Begin/
-# InnerTxnBuilder.Begin(),
-#             InnerTxnBuilder.MethodCall(
-#                 app_id=app_id,
-#                 method_signature=get_method_signature(yourcontracttocall.methodname),
-#                 args=[asset_id],
-#             ),
-#             InnerTxnBuilder.Next(),
-#             ... 
+    @external
+    def oneself(self, *, output: abi.String):
+        return Seq(
+            InnerTxnBuilder.ExecuteMethodCall(
+                app_id=Int(1304),
+                method_signature="one()uint8",
+                args=[],
+            ),
+            output.set("ok")
+        )
+    
+    @external
+    def nick_matches(self, nick: Nickname, app_id: abi.Uint64, *, output: Nickname):
+        # pushbytes 0x4bac4e0b // "nick_matches(byte[8],uint64,string)void"
+        return Seq(
+            InnerTxnBuilder.ExecuteMethodCall(
+                app_id=app_id.get(),
+                # method_signature="get_nick()uint8",
+                method_signature=get_method_signature(AlgoBlog.get_nick),
+                args=[],
+            ),
+            # Assert(BytesEq(retval, nick.get())),
+            Assert(BytesEq(nick.get(), Suffix(InnerTxn.last_log(), Int(4)))),
+            output.decode(Suffix(InnerTxn.last_log(), Int(4)))
+        )    
+
+def deploy_checker():
+    deploy = 1
+
+    if deploy:
+        # Create an Application client
+        app_client = client.ApplicationClient(
+            # Get sandbox algod client
+            client=sandbox.get_algod_client(),
+            # Instantiate app with the program version (default is MAX_TEAL_VERSION)
+            app=Checker(version=8),
+            # Get acct from sandbox and pass the signer
+            signer=sandbox.get_accounts().pop().signer,
+        )
+
+        # Deploy the app on-chain
+        app_id, app_addr, txid = app_client.create()
+        print(
+            f"""Deployed app in txid {txid}
+            App ID: {app_id} 
+            Address: {app_addr} 
+        """
+        )
+    else:
+        app_addr = 'IKFUUZDNRXCZSVEWGFKEGSNHXBYXFFBMYRQP6X3EOTEAD4L3NHWCSSFKNQ'
+        app_client = client.ApplicationClient(
+            # Get sandbox algod client
+            client=sandbox.get_algod_client(),
+            # Instantiate app with the program version (default is MAX_TEAL_VERSION)
+            app=Checker(version=8),
+            app_id=1191,
+            # Get acct from sandbox and pass the signer
+            signer=sandbox.get_accounts().pop().signer,
+        )
+
+    boxen = lambda x: [[app_client.app_id, name] for name in x]
+    app_client.fund(517000, app_addr) # app_client.fund(108100, app_addr)
+    return app_client
+
+def demo_checker():
+    app_client = deploy_checker()
+    boxen = lambda x: [[app_client.app_id, name] for name in x]
+
+    result = app_client.call(Checker.oneself, foreign_apps=[1304])
+    print("check oneself: ", result.return_value) 
+
+    result = app_client.call(Checker.nick_matches, 
+                             foreign_apps=[1391],
+                             boxes=[[1391, "nick"]],
+                            #  boxes=boxen(["username", "nick"]),
+                             nick=_nick(b'tomo'), app_id=1391
+                            )
+    print("nick_matches: ", result.return_value) 
 
 # Create a class, subclassing Application from beaker
 class AlgoBlog(Application):
@@ -145,7 +231,15 @@ class AlgoBlog(Application):
             App.box_put(Bytes("username"), username.get()),
             output.set(username.get())
         )
-     
+    
+    @external
+    def get_nick(self, *, output: Nickname):
+        return Seq(
+            nick := App.box_get(Bytes("nick")),
+            Assert(nick.hasValue()),
+            output.set(nick.value())
+        )
+    
     @external
     def idLast_reset(self, *, output: abi.String):
         return Seq(
@@ -191,8 +285,8 @@ class AlgoBlog(Application):
             output.set(SetByte(Bytes("id:?"), Int(3), Int(0x30) + (Int(1) + Btoi(last.value())) % Int(10)))
         )
 
-def demo():
-    deploy = 0
+def deploy_blog():
+    deploy = 1
 
     if deploy:
         # Create an Application client
@@ -228,9 +322,17 @@ def demo():
     boxen = lambda x: [[app_client.app_id, name] for name in x]
     app_client.fund(517000, app_addr) # app_client.fund(108100, app_addr)
 
+    return app_client
+
+def demo():
+    app_client = deploy_blog()
+    boxen = lambda x: [[app_client.app_id, name] for name in x]
+
     result = app_client.call(AlgoBlog.init, boxes=boxen(["username", "nick"]),
                              nick=_nick(b'tomo'), username=_username(b'tomo'))
     print("created account: ", result.return_value) 
+    result = app_client.call(AlgoBlog.get_nick, boxes=boxen(["nick"]))
+    print("Confirm nick set: ", result.return_value)
 
     result = app_client.call(AlgoBlog.idLast_reset, boxes=boxen(["idLast"]))
     pprint(vars(result))
@@ -278,5 +380,6 @@ def demo():
 
 
 if __name__ == "__main__":
-    demo()
+    # demo()
+    demo_checker()
     # regdemo()
